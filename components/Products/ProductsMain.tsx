5a940d3e70521c830e6e328b7583017867377e4b
hac1yev <ilkinhaciyev955@gmail.com>
2025-05-16 15:13:56 +0400

    products page navigate searchParams problem fixed

--- components/Products/ProductsMain.tsx
+++ components/Products/ProductsMain.tsx
@@ -4,13 +4,16 @@ import ProductsFilterSelect from "./ProductsFilterSelect";
 import CardItem from "../Cards/CardItem";
 import { useTypedProductSelector } from "@/store/products-slice";
 import ProductsPagination from "./ProductsPagination";
+import { Suspense } from "react";
 
 const ProductsMain = () => {
-  const products = useTypedProductSelector((state) => state.productReducer.products);
+  const products = useTypedProductSelector(
+    (state) => state.productReducer.products
+  );
 
   // const [isModalOpen,setIsModalOpen] = useState(false);
   // const [modalData,setModalData] = useState<ProductType>();
-  
+
   // const handleOpenModal = useCallback((id: string) => {
   //     setIsModalOpen(true);
   //     const findedProduct = products.find((product) => product.id === parseInt(id));
@@ -21,37 +24,38 @@ const ProductsMain = () => {
   //     setIsModalOpen(false);
   // }, []);
 
-
   return (
-    <div className="col-xl-9 col-lg-12 products-main-wrapper">
-      <ProductsFilterSelect />
-      <div
-        className="product-area-wrapper-shopgrid-list mt--20 tab-pane fade show active"
-        id="home-tab-pane"
-      >
-        <div className="row g-4">
-          {products.map((product) => (
-            <div
-              className="col-lg-3 col-md-6 col-sm-6 col-12"
-              key={product.id}
-            >
-              <CardItem 
+    <Suspense>
+      <div className="col-xl-9 col-lg-12 products-main-wrapper">
+        <ProductsFilterSelect />
+        <div
+          className="product-area-wrapper-shopgrid-list mt--20 tab-pane fade show active"
+          id="home-tab-pane"
+        >
+          <div className="row g-4">
+            {products.map((product) => (
+              <div
+                className="col-lg-3 col-md-6 col-sm-6 col-12"
                 key={product.id}
-                // handleOpenModal={handleOpenModal}
-                id={product.id}
-                discount={product.discount}
-                image={product.image}
-                title={product.title}
-                price={product.price}
-                value={product.value}
-                brand={product.brand}
-              />
-            </div>
-          ))}
+              >
+                <CardItem
+                  key={product.id}
+                  // handleOpenModal={handleOpenModal}
+                  id={product.id}
+                  discount={product.discount}
+                  image={product.image}
+                  title={product.title}
+                  price={product.price}
+                  value={product.value}
+                  brand={product.brand}
+                />
+              </div>
+            ))}
+          </div>
         </div>
+        <ProductsPagination />
       </div>
-      <ProductsPagination />
-    </div>
+    </Suspense>
   );
 };
 
